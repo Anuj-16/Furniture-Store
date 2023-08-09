d0bf989273d301a116d74b3b1b3e31ac8e38861d
unknown <vedican.v44@gmail.com>
2023-08-09 09:59:03 +0530

    ho gya

--- furniture/src/index.js
+++ furniture/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { ChakraProvider, theme } from "@chakra-ui/react";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
