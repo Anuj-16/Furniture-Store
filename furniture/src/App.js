4bc9abb293b7d09ebde79f79977aad6e8bf8c179
pankajsbisht143 <pankajsbisht143@gmail.com>
2023-08-09 14:26:26 +0530

    done

--- furniture/src/App.js
+++ furniture/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Product from "./cartpage/Product"
+import Payment from "./cartpage/Payment"
 
 function App() {
   return (
     <div className="App">
-     
+     {/* <Product/> */}
+     <Payment/>
     </div>
   );
 }
