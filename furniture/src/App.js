18d17986b976ca93ab3af4f08ff2160c76913d9f
unknown <vedican.v44@gmail.com>
2023-08-12 07:09:02 +0530

    ho gya

--- furniture/src/App.js
+++ furniture/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Home from './components/homePage/home'; // Make sure the path is correct and the component is named properly
+// import SecondNav22 from "./components/homePage/navbar"
 function App() {
   return (
     <div className="App">
-    
+      
+      <Home /> 
+      
     </div>
   );
 }
