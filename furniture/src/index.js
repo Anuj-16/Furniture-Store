42e503de6e87cba8c7c3810f5823c55ecd955558
Sanjeev Kushwaha <sanjeevk249116@gmail.com>
2023-08-13 22:30:41 +0530

    add

--- furniture/src/index.js
+++ furniture/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
+
 import {BrowserRouter} from "react-router-dom"
 import { Provider } from "react-redux";
 import {store} from "./redux/store"
@@ -17,17 +17,7 @@ root.render(
 </AuthContentProvider>
   
  
-=======
 
-import { ChakraProvider, theme } from "@chakra-ui/react";
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>,
-// >>>>>>> 34b6c7e21604cb1ae40eef1064aaf343a6c1297e
->>>>>>> d9758b5ca688b41bc7b9f03750093e70aa2ac6f1
 );
 
 // If you want to start measuring performance in your app, pass a function
