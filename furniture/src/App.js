4b86482dc198ccef9d0a589c4d6221d945225f34
Sanjeev Kushwaha <sanjeevk249116@gmail.com>
2023-08-15 11:32:24 +0530

    add

--- furniture/src/App.js
+++ furniture/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './App.css';
-import Nav1 from './components/Nav1';
-import RouterMain from './Routers/RouterMain';
-import Footer from './components/homePage/footer';
+
 
 
 function App() {
   return (
     <div className="App">
-     <Nav1/>
-     <RouterMain/>
-     <Footer/>
+   
     </div>
   );
 }
